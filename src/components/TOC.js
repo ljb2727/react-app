7e6d5483e7c09d00970e66123c9aebdc179f4d55
ljb2727 <ljb2727@gmail.com>
2020-02-18 17:01:19 +0900

    react-app first commit

---
+++ src/components/TOC.js
@@ -0,0 +1,19 @@
+
+import React, { Component } from 'react';
+
+class TOC extends Component {
+    render(){
+      return (
+        <nav>
+            <ul>
+                <li>
+                    <a href="1.html">a</a>
+                    <a href="2.html">b</a>
+                    <a href="3.html">c</a>
+                </li>
+            </ul>
+        </nav>
+      )
+    }
+  }
+  export default TOC;
