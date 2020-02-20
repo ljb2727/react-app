d7bfb1b2b815a10d3e3fbe73e23a36ad2547968e
ljb2727 <ljb2727@gmail.com>
2020-02-20 10:47:28 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import './App.css';
-import Subject from './components/Subject'
-import TOC from './components/TOC'
-import Content from './components/Content'
-
-
+import React, { Component } from "react";
+import "./App.css";
+import Subject from "./components/Subject";
+import TOC from "./components/TOC";
+import Content from "./components/Content";
+import Test from "./components/Test";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      <Subject title="web" sub="word"></Subject>
-      <TOC></TOC>
-      <Content title="title" desc="desc"></Content>
-    </div>
-    )
-  }
+    render() {
+        return (
+            <div className="App">
+                <Subject title="web" sub="word"></Subject>
+                <TOC></TOC>
+                <Content title="title" desc="desc"></Content>
+                <Test></Test>
+            </div>
+        );
+    }
 }
 
 export default App;
